c8de20fa7990649502bfac4fbb5d1a768b266938
llucasbruno <lucasitalia394@gmail.com>
2024-04-15 11:39:57 -0300

    Background animation completed and menu finished

--- js/app.js
+++ js/app.js
@@ -11,67 +11,109 @@ function activateMenu() {
 activateMenu();
 */
 
-function scrollHeader(){
-    const nav = document.querySelector('.bg-header');
+function scrollHeader() {
+    const nav = document.querySelector(".bg-header");
 
-    if(this.scrollY >= 200){
-        nav.classList.add('active-header');
+    if (this.scrollY >= 200) {
+        nav.classList.add("active-header");
     } else {
-        nav.classList.remove('active-header')
+        nav.classList.remove("active-header");
     }
 }
 
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
-
-const menu = document.querySelector('.menu');
+const menu = document.querySelector(".menu");
 //const menuHamburguer = document.querySelector('.menu-hamburguer');
 
+/*--=========== TIMELINE MENU ============- */
 
-            /*--=========== TIMELINE MENU ============- */
-            const bx = document.querySelector('.menu-hamburguer');
-            gsap.set(".menu", {x: '-100%', opacity: 0});
+const bx = document.querySelector(".bx");
+gsap.set(".menu", { x: "-100%", opacity: 0 });
 
-            var tl2 = gsap.timeline({paused: true});
+var tl2 = gsap.timeline({ paused: true });
 
-            tl2.to(".menu", 1.1, {
-                x: 0, 
-                opacity: 1, 
-                ease: "power4.out"
-            })
-            .from(".nav-item", 1,{
-                x: -100, 
-                opacity: 0, 
-                //filter: 'blur(10px)',
-                stagger:{each: 0.1}, 
-                ease: "power4.out"
-            }, '-=0.5')
-            .from(".grid-menu img", 0.5,{
-                x: -100, 
-                opacity: 0, 
-                //filter: 'blur(10px)',
-                ease: "power4.out"
-            }, '-=0.4')
-            .from(".grid-menu h4", 0.5,{
-                x: -100, 
-                opacity: 0, 
-                //filter: 'blur(10px)',
-                ease: "power4.out"
-            }, '-=0.3')
-            .from(".grid-menu span", 0.5,{
-                x: -100, 
-                opacity: 0, 
-                ease: "power4.out"
-            }, '-=0.3')
-            .from(".grid-icons a", 0.5,{
-                opacity: 0, 
-                stagger:{ each: 0.1}, 
-                ease: "power4.out"
-            }, '-=0.25')
+tl2
+    .to(".menu", 1.1, {
+        x: 0,
+        opacity: 1,
+        ease: "power4.out",
+    })
+    .from(
+        ".nav-item",
+        1,
+        {
+            x: -100,
+            opacity: 0,
+            //filter: 'blur(10px)',
+            stagger: { each: 0.1 },
+            ease: "power4.out",
+        },
+        "-=0.5"
+    )
+    .from(
+        ".grid-icons-menu div",
+        0.5,
+        {
+            x: -100,
+            opacity: 0,
+            //filter: 'blur(10px)',
+            ease: "power4.out",
+        },
+        "-=0.4"
+    )
+    .from(
+        ".grid-icons-menu h4",
+        0.5,
+        {
+            x: -100,
+            opacity: 0,
+            //filter: 'blur(10px)',
+            ease: "power4.out",
+        },
+        "-=0.3"
+    )
+    .from(
+        ".grid-icons-menu span",
+        0.5,
+        {
+            x: -100,
+            opacity: 0,
+            ease: "power4.out",
+        },
+        "-=0.3"
+    )
+    .from(
+        ".grid-icons-menu  a",
+        0.5,
+        {
+            opacity: 0,
+            stagger: { each: 0.1 },
+            ease: "power4.out",
+        },
+        "-=0.25"
+    );
 
-            tl2.reverse();
+tl2.reverse();
 
-            bx.addEventListener('click', () => {
-                bx.classList.toggle('active');
-                tl2.reversed(!tl2.reversed());
-            });
+bx.addEventListener("click", () => {
+    bx.classList.toggle("active");
+    tl2.reversed(!tl2.reversed());
+});
+
+
+
+function toggleClass(selector, className) {
+    const elemento = document.querySelector(selector);
+
+    if (elemento) {
+        elemento.classList.toggle(className);
+    }
+}
+
+setInterval(() => toggleClass('.c1', 'c1-move'), 2500);
+setInterval(() => toggleClass('.c2', 'c2-move'), 4000);
+setInterval(() => toggleClass('.c3', 'c3-move'), 5000);
+setInterval(() => toggleClass('.c4', 'c4-move'), 5000);
+
+
