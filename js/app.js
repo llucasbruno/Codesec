08c5df95ba5e8d4bd7e8e53faf2935412c17ba06
llucasbruno <lucasitalia394@gmail.com>
2024-11-06 17:57:04 -0300

    Pre-launch improvements

--- js/app.js
+++ js/app.js
@@ -177,3 +177,88 @@ $(document).ready(function() {
     
 });
     */
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const acordeonButtons = document.querySelectorAll('.acordeon__button');
+
+    acordeonButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
+            const acordeon = button.parentElement; // 
+
+            button.classList.toggle('active');
+            acordeon.classList.toggle('active'); // 
+            if (button.classList.contains('active')) {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } else {
+                content.style.maxHeight = 0;
+            }
+        });
+    });
+});
+
+
+
+
+
+
+// Selecionar elementos
+let bar = document.querySelector('.bar');
+
+// Função para arrastar o elemento
+function dragElement(elmnt) {
+    let dist = 0, currentPos = 0;
+    let initialPos = elmnt.getBoundingClientRect().left;
+
+    // Definir a posição inicial no estilo para iniciar a transição
+    elmnt.style.left = initialPos + "px";
+
+    // Adicionar evento de mouse down
+    elmnt.addEventListener('mousedown', dragMouseDown);
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        currentPos = e.clientX;
+
+        // Remover a transição para uma movimentação mais suave
+        elmnt.style.transition = 'none';
+
+        // Adicionar eventos de mouse up e mouse move
+        document.addEventListener('mouseup', closeDragElement);
+        document.addEventListener('mousemove', elementDrag);
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+
+        dist = currentPos - e.clientX;
+        currentPos = e.clientX;
+
+        let updatedPos = elmnt.offsetLeft - dist;
+
+        // Garantir que o elemento não saia dos limites da tela
+        if (updatedPos < 0) {
+            updatedPos = 0;
+        } else if (updatedPos > window.innerWidth - elmnt.offsetWidth) {
+            updatedPos = window.innerWidth - elmnt.offsetWidth;
+        }
+
+        elmnt.style.left = updatedPos + "px";
+    }
+
+    function closeDragElement() {
+        // Remover eventos de mouse up e mouse move
+        document.removeEventListener('mouseup', closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
+
+        // Aplicar a transição para retornar à posição inicial
+        elmnt.style.transition = 'left 0.5s ease';
+        elmnt.style.left = initialPos + "px";
+    }
+}
+
+// Inicializar arrastar no elemento
+dragElement(bar);
+
+
